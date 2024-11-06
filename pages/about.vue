9c1253790ce3e21c7cb3f7018ca90e6d9a4bc3f3
imnothere999 <imnotherex@gmail.com>
2024-11-05 20:33:36 -0600

    just do it

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,8 @@
 <template>
 	<PrimePanel header="About Me" class="my-6">
-		About me stuffs
+		About me stuffs WHat the fu-------- why not workie
+		Pleas actually change
+		<p>Why doesn't it show changes when I want it to this is knid of bull. I need to line wrap multiple times so lets see if this works at all or if it is fu----g stupid<b>this is bolded fs</b><i> for real there are italics here its not a figment of my imagination</i></p>
 		<PrimeButton label="Check" icon="pi pi-check" />
 	</PrimePanel>
 </template>
