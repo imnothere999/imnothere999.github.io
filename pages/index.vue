5716b02da73453b678903362d596f7d4eb5dbcdd
imnothere999 <imnotherex@gmail.com>
2024-11-05 20:21:06 -0600

    Testing initial changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-	<PrimePanel header="Welcome!" class="my-6">
-		Hello World!
-		<PrimeButton label="Check" icon="pi pi-check" />
+
+	<h1>That was some good head</h1>
+
+	<head>
+		<link rel = "stylesheet" type="text/css" href="fancy.css">
+		<title>Tittles</title>
+	</head>
+	<body>
+		<PrimePanel header="Welcome!" class="my-6">
+			Hello World!
+			<p><b>Lorem ipsum:</b>Lorem ipsum dolor sit amet, <i>consectetur</i> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p></p>
+		
+			<PrimeButton label="Check" icon="pi pi-check" />
 	</PrimePanel>
+	</body>
+	
 </template>
