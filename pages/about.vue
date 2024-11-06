f4d3df46a9c99a67148460cffe04072342132515
imnothere999 <imnotherex@gmail.com>
2024-11-06 16:57:40 -0600

    Added resume

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,11 @@
 <template>
-
-	<head></head>
+	<h1>My Favorite Anime:</h1>
+	<head>
+		<title>About Me?</title>
+		<link rel="stylesheet" type="text/css" href="fancy.css">
+	</head>
 
 	<body>
-		<h2>My Favorite Anime:</h2>
-		
 		<ol>
 			<li>1. Monster</li>
 			<li>2. Your Lie in April</li>
@@ -12,12 +13,12 @@
 			<li>4. So Long Mr. Despair</li>
 			<li>5. Bleach</li>
 		</ol>
-
-		<p>This song pops off:</p>
+		
+		<h3>W asmongold:</h3>
 		 
-		<iframe width="420" height="315" src="https://www.youtube.com/watch?v=-DIx8uinOAA"></iframe>
+		<iframe width="420" height="315" src="https://www.youtube.com/watch?v=Z82wr3Fgcb8"></iframe>
 
-		<h1>here is a picture of the guy from my fav anime:</h1>
+		<h3>here is a picture of the guy from my fav anime:</h3>
 
 		<img src="assets/tenma.jpg" alt="Kenzo Tenma" />
 
--- pages/resume.vue
+++ pages/resume.vue
@@ -1,9 +1,20 @@
 
 <template>
-    <PrimePanel header="Welcome!" class="my-6">
-        Lorem ipsum resumum
-    
-        <PrimeButton label="Check" icon="pi pi-check" />
-</PrimePanel>
+    <h1>Yuke Wiruhooufuto</h1>
+    <head>
+        <title>ResUMe</title>
+        
+        <link rel="stylesheet" type="text/css" href="fancy.css">
+    </head>
+    <head>
+        <h1><b>Contact: </b></h1>
+        <p>+1 (913) 420 6969 (Call Me)</p>
+        <h1><b>Education: </b></h1>
+        <p>I was homeschooled </p>
+        <h1><b>Work Experience: </b></h1>
+        <p>I worked in fast food!!?!?!?!?!???!</p>
+        <h1><b>Skills: </b></h1>
+        <p style="text-indent: 15px;">I play some epic pianos</p>
+    </head>>
 
 </template>
