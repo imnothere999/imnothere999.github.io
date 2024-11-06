cdf59caf72b5aad8efc4e5a262db7ce4961538d0
imnothere999 <imnotherex@gmail.com>
2024-11-06 17:52:02 -0600

    fixed???????????????????????no

--- pages/resume.vue
+++ pages/resume.vue
@@ -4,7 +4,7 @@
     <head>
         <title>ResUMe</title>
         
-        <link rel="stylesheet" type="text/css" href="css/fancy.css">
+        <link rel="stylesheet" type="text/css" href="assets/fancy.css">
     </head>
 
         <h1>
